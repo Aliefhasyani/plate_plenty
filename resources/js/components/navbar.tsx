1c611320da93ec28a8b4038e34283c570fffd722
Aliefhasyani <aliefhasyani05@gmail.com>
2025-10-31 22:34:10 +0800

    feat.initial commit

---
+++ resources/js/components/navbar.tsx
@@ -0,0 +1,16 @@
+
+function Navbar(){
+    return(
+        <>
+            <nav>
+                <div className="bg-[#ABE7B2] flex items-center justify-center text-center gap-25 w-auto h-20 font-[Quicksand] text-2xl font-bold">
+                    <a href="" className="text-[#F9F5F0] hover:underline">HOME</a>
+                    <a href="" className="text-[#F9F5F0] hover:underline">RECIPES</a>
+                    <a href="" className="text-[#F9F5F0] hover:underline">ABOUT</a>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
---
+++ resources/js/pages/homepage.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar";
+
+
+function Homepage(){
+    return(
+        <>
+        <Navbar></Navbar>
+            <div>
+                <h1 className="text-blue-600">WELCOME TO HOMEPAGE</h1>
+                
+            </div>
+        </>
+    )
+}
+
+export default Homepage;
